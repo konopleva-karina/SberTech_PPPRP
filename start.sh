e552732f44b10460edf42566e12e0117e6288a71
Karina Konopleva <konopleva2001karina@gmail.com>
2023-04-26 05:54:41 +0300

    fixed dirs in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-kubectl apply -f myapp/flask/flask.yaml
-kubectl apply -f myapp/flask/gateway.yaml
-kubectl apply -f myapp/flask/external.yaml
-kubectl apply -f myapp/nginx/nginx.yaml
-kubectl apply -f myapp/nginx/gateway.yaml
+kubectl apply -f flask/flask.yaml
+kubectl apply -f flask/gateway.yaml
+kubectl apply -f flask/external.yaml
+kubectl apply -f nginx/nginx.yaml
+kubectl apply -f nginx/gateway.yaml
